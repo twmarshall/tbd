0fcd31719895e64bbdefc1f5aaeba8361829a8a0
Thomas Marshall <twmarsha@andrew.cmu.edu>
2014-05-22 15:11:35 -0400

    Fixed compilation issue with WordcountTest.

--- examples/src/test/scala/tbd/WordcountTests.scala
+++ examples/src/test/scala/tbd/WordcountTests.scala
@@ -29,7 +29,8 @@ class WordcountTests extends FlatSpec with Matchers {
     mutator.put(2, "cat boy boy ear cat dog")
     mutator.put(3, "ear cat apple")
 
-    val output = mutator.run[Mod[(Int, HashMap[String, Int])]](new WCAdjust(2, false))
+    val output =
+      mutator.run[Mod[(Int, HashMap[String, Int])]](new WCAdjust(2, 0, false))
     val answer = HashMap[String, Int]("apple" -> 3, "boy" -> 3, "cat" -> 6,
                                       "dog" -> 1, "ear" -> 2)
     assert(output.read()._2 == answer)
