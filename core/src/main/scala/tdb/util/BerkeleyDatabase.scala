51115009ab3dab3b2d6972941568891685d767c9
Thomas Marshall <twmarsha@andrew.cmu.edu>
2015-02-08 20:00:09 -0500

    Fixed the issue with keying on page titles by increasing BerkeleyDB's cache size.

--- core/src/main/scala/tdb/util/BerkeleyDatabase.scala
+++ core/src/main/scala/tdb/util/BerkeleyDatabase.scala
@@ -20,7 +20,7 @@ import java.io.{File, FileNotFoundException}
 
 class BerkeleyDatabase {
   private val envConfig = new EnvironmentConfig()
-  envConfig.setCacheSize(96 * 1024)
+  envConfig.setCacheSize(96 * 1024 * 1024)
   envConfig.setAllowCreate(true)
 
   private val envHome = new File("/tmp/tdb_berkeleydb")
--- core/src/main/scala/tdb/util/BerkeleyInputStore.scala
+++ core/src/main/scala/tdb/util/BerkeleyInputStore.scala
@@ -16,8 +16,9 @@
 package tdb.util
 
 import com.sleepycat.je.Environment
-import com.sleepycat.persist.{EntityStore, PrimaryIndex, SecondaryIndex, StoreConfig}
+import com.sleepycat.persist._
 import com.sleepycat.persist.model.{Entity, PrimaryKey, SecondaryKey}
+import com.sleepycat.persist.model.Relationship.ONE_TO_ONE
 import java.io._
 import scala.collection.mutable.{Buffer, Map}
 
@@ -26,66 +27,42 @@ class BerkeleyInputStore(environment: Environment, name: String) {
   storeConfig.setAllowCreate(true)
 
   private val stores = Buffer[EntityStore]()
-  private val indexes = Buffer[PrimaryIndex[Integer, InputEntity]]()
-  private val keyStores = Buffer[EntityStore]()
-  private val keyIndexes = Buffer[PrimaryIndex[Integer, KeyEntity]]()
-
-  private val keys = Map[String, Integer]()
+  private val indexes = Buffer[PrimaryIndex[String, InputEntity]]()
 
   for (i <- 1 to 12) {
     val store = new EntityStore(environment, name + i, storeConfig)
     stores += store
 
-    indexes += store.getPrimaryIndex(classOf[Integer], classOf[InputEntity])
-
-    val keyStore = new EntityStore(environment, name + "Key" + i, storeConfig)
-    stores += keyStore
-
-    keyIndexes += keyStore.getPrimaryIndex(classOf[Integer], classOf[KeyEntity])
+    val index = store.getPrimaryIndex(classOf[String], classOf[InputEntity])
+    indexes += index
   }
 
   private def hashKey(key: String) = {
     key.hashCode().abs % indexes.size
   }
 
-  var nextId = 0
   def put(key: String, value: String) {
     val entity = new InputEntity()
-    entity.id = nextId
-    nextId += 1
+    entity.key = key
     entity.value = value
 
     val hash = hashKey(key)
     indexes(hash).put(entity)
-
-    val keyEntity = new KeyEntity()
-    keyEntity.id = entity.id
-    keyEntity.key = key
-
-    keyIndexes(hash).put(keyEntity)
-
-    keys(key) = entity.id
   }
 
   def get(key: String) = {
-    val entity = indexes(hashKey(key)).get(keys(key))
+    val entity = indexes(hashKey(key)).get(key)
     (key, entity.value)
   }
 
-  def delete(key: String) {
-    indexes(hashKey(key)).delete(keys(key))
-  }
-
   def iterateInput(process: Iterable[String] => Unit, partitions: Int) {
-    val cursors = keyIndexes.map(_.entities())
+    val keyCursors = indexes.map(_.keys())
 
     val buf = Buffer[String]()
-    for (cursor <- cursors) {
+    for (cursor <- keyCursors) {
       val it = cursor.iterator
       while (it.hasNext) {
-        val keyEntity = it.next()
-        buf += keyEntity.key
-        keys(keyEntity.key) = keyEntity.id
+        buf += it.next()
       }
 
       process(buf)
@@ -94,8 +71,6 @@ class BerkeleyInputStore(environment: Environment, name: String) {
       cursor.close()
     }
   }
-
-  def contains(key: String) = indexes(hashKey(key)).contains(keys(key))
 
   def count() = indexes.map(_.count()).reduce(_ + _)
 
@@ -107,17 +82,9 @@ class BerkeleyInputStore(environment: Environment, name: String) {
 }
 
 @Entity
-class KeyEntity {
-  @PrimaryKey
-  var id: Integer = -1
-
-  var key: String = ""
-}
-
-@Entity
 class InputEntity {
   @PrimaryKey
-  var id: Integer = -1
+  var key: String = ""
 
   var value: String = ""
 }
