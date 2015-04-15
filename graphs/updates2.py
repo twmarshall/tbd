6526841bf836d8ea6a7382c518c4da615d53bf8c
Thomas Marshall <twmarsha@andrew.cmu.edu>
2015-04-15 13:35:26 -0400

    Some tweaks to updates2

--- graphs/updates2.py
+++ graphs/updates2.py
@@ -29,17 +29,15 @@ for line in file:
     else:
         labels.append("Update " + label)
 
-    load.append(float(split[1]))
-    gc.append(float(split[2]))
-    rest.append(float(split[3]))
-    total.append(float(split[4]))
-    std.append(float(split[5]))
+    load.append(float(split[1]) / 1000)
+    gc.append(float(split[2]) / 1000)
+    rest.append(float(split[3]) / 1000)
+    total.append(float(split[4]) / 1000)
+    std.append(float(split[5]) / 1000)
 
 width = .5
 
 x = np.arange(len(load)) + .5  # the x locations for the groups
-#for i in 
-#    x.append(i + 1)
 
 fig, ax = plt.subplots()
 
@@ -51,14 +49,11 @@ loadBar = ax.bar(x, load, width, color='#414FAF', hatch='/')
 gcBar = ax.bar(x, gc, width, color='#FF6833', bottom=load, hatch='-')
 restBar = ax.bar(x, rest, width, color='#8BC34A', bottom=gcLoad, hatch='X', yerr=std)
 
-# Labeling
-ax.set_title('Initial Setup')
-
 ax.set_xticks(x + width/2)
 ax.set_xticklabels(labels)
 
 ax.set_ylabel('Time (seconds)')
-ax.set_ylim(bottom = 0, top = 15000)
+ax.set_ylim(bottom = 0, top = 15)
 ax.set_xlim(0, 2.5)
 #ax.set_yscale("log")
 
