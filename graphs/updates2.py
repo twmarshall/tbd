71ff70a6a3b8d4cc88dce738548e5a517911053d
Thomas Marshall <twmarsha@andrew.cmu.edu>
2015-05-05 16:39:59 -0400

    Added some stuff to updates2.py

--- graphs/updates2.py
+++ graphs/updates2.py
@@ -12,6 +12,9 @@ graph_dir = "graphs/"
 
 # Load results
 file = open("results.txt", 'r')
+
+scale = 1
+
 labels = []
 load = []
 gc = []
@@ -25,15 +28,15 @@ for line in file:
     if label == "initial":
         labels.append("Initial Run")
     elif label == "naive":
-        labels.append("Non-incremental")
+        labels.append("Nonincremental")
     else:
         labels.append("Update " + label)
 
-    load.append(float(split[1]) / 1000)
-    gc.append(float(split[2]) / 1000)
-    rest.append(float(split[3]) / 1000)
-    total.append(float(split[4]) / 1000)
-    std.append(float(split[5]) / 1000)
+    load.append(float(split[1]) / scale)
+    gc.append(float(split[2]) / scale)
+    rest.append(float(split[3]) / scale)
+    total.append(float(split[4]) / scale)
+    std.append(float(split[5]) / scale)
 
 width = .5
 
@@ -45,19 +48,20 @@ gcLoad = []
 for i in range(0, len(load)):
     gcLoad.append(load[i] + gc[i])
 
-loadBar = ax.bar(x, load, width, color='#414FAF', hatch='/')
-gcBar = ax.bar(x, gc, width, color='#FF6833', bottom=load, hatch='-')
-restBar = ax.bar(x, rest, width, color='#8BC34A', bottom=gcLoad, hatch='X', yerr=std)
+#loadBar = ax.bar(x, load, width, color='#414FAF', hatch='/', log=True)
+#gcBar = ax.bar(x, gc, width, color='#FF6833', bottom=load, hatch='-', log=True)
+#restBar = ax.bar(x, rest, width, color='#8BC34A', bottom=gcLoad, hatch='X', yerr=std, log=True)
+totalBar = ax.bar(x, total, width, color='#414FAF', hatch='/', log=True)
 
 ax.set_xticks(x + width/2)
 ax.set_xticklabels(labels)
 
 ax.set_ylabel('Time (seconds)')
-ax.set_ylim(bottom = 0, top = 15)
-ax.set_xlim(0, 2.5)
+ax.set_ylim(bottom = 1)
+ax.set_xlim(0, len(load) + .5)
 #ax.set_yscale("log")
 
-ax.legend( (loadBar[0], gcBar[0], restBar[0]), ('Load', 'GC', 'Execution') )
+#ax.legend( (loadBar[0], gcBar[0], restBar[0]), ('Load', 'GC', 'Execution') )
 
 def autolabel(rects):
     # attach some text labels
@@ -66,9 +70,10 @@ def autolabel(rects):
         ax.text(rect.get_x()+rect.get_width()/2., 1.01 * height, '%d'%int(height),
                 ha='center', va='bottom')
 
-autolabel(restBar)
+#autolabel(restBar)
+autolabel(totalBar)
 
 if len(sys.argv) > 1:
     plt.savefig(graph_dir + sys.argv[1])
 else:
-    plt.savefig(graph_dir + "updates2.png")
+    plt.savefig(graph_dir + "updates2.svg")
